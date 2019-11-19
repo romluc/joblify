e9c930be2dc15d24dc46b8f32468afdd1d666455
romluc <romulofilhofilho@gmail.com>
2019-11-19 15:32:06 -0300

    Add ejs as view engine

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello FSL!');
+});
+
+const port = 3000;
+app.listen(port, err => {
+  if (err) {
+    console.log('Nao foi possivel rodar o servidor!');
+  } else {
+    console.log(`Servidor rodando na porta ${port}`);
+  }
+});
